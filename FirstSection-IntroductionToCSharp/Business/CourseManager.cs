42024d0a8b12e6c0a80db1b84fd649825d0a30ae
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-03-30 21:53:52 +0300

    Add project files.

---
+++ FirstSection-IntroductionToCSharp/Business/CourseManager.cs
@@ -0,0 +1,44 @@
+﻿using FirstSection_IntroductionToCSharp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSection_IntroductionToCSharp.Business;
+
+public class CourseManager
+{
+    Course[] courses = new Course[3];
+
+    // Constructor
+    public CourseManager()
+    {
+        Course course1 = new Course();
+        course1.Id = 1;
+        course1.Name = "C#";
+        course1.Description = "C# 8 vs...";
+        course1.Price = 0;
+
+        Course course2 = new Course();
+        course2.Id = 1;
+        course2.Name = "JAVA";
+        course2.Description = "Java 17 vs...";
+        course2.Price = 10;
+
+        Course course3 = new Course();
+        course3.Id = 1;
+        course3.Name = "PYTHON";
+        course3.Description = "Python 3.12... vs...";
+        course3.Price = 20;
+
+        courses[0] = course1;
+        courses[1] = course2;
+        courses[2] = course3;
+    }
+
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
---
+++ FirstSection-IntroductionToCSharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using FirstSection_IntroductionToCSharp.Business;
+using FirstSection_IntroductionToCSharp.Entities;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("__________________________________________________");
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+// Variables --> camelCase
+bool isAuthenticated = false;
+
+Console.WriteLine(message1);
+
+// Condition
+if (isAuthenticated)
+{
+    Console.WriteLine("Buton --> Hoşgeldin Enes");
+}
+else
+{
+    Console.WriteLine("Buton --> Sisteme Giriş Yap");
+}
+
+string kredi1 = "Kredi 1";
+string kredi2 = "Kredi 2";
+string kredi3 = "Kredi 3";
+string kredi4 = "Kredi 4";
+string kredi5 = "Kredi 5";
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4", "Kredi 5" };
+// string[] loans = new string[5];
+// loans[0] = "Kredi 1";
+
+//start    //condition       //increment
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+Console.WriteLine("__________________________________________________");
+CourseManager courseManager = new();
+Course[] courses = courseManager.GetAll();
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " --> " + courses[i].Price);
+}
